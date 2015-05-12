2ae7f2162c61521760a9d642581b34d4194cd946
Rosa Lu Zhan <rosaluzhan@gmail.com>
2015-05-12 19:57:09 +0200

    autoload hecho

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,14 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
+
+// Definición de rutas de /quizes
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',quizController.answer);
 router.get('/quizes/author', quizController.author);
 
 module.exports = router;
