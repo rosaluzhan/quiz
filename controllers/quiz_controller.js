845dff4e7115be5eac4657cd6490206c2e77a08f
Rosa Lu Zhan <rosaluzhan@gmail.com>
2015-05-25 22:42:01 +0200

    quiz23: imagenes

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -31,15 +31,6 @@ exports.load = function(req, res, next, quizId) {
   ).catch(function(error){next(error)});
 };
 
-// GET /quizes
-//exports.index = function(req, res) {
-//  models.Quiz.findAll().then(
-//    function(quizes) {
-//      res.render('quizes/index.ejs', {quizes: quizes, errors: []});
-//    }
-//  ).catch(function(error){next(error)});
-//};
-
 // GET /quizes/question
 exports.index = function(req, res) {
   if (req.query.search) {
@@ -52,8 +43,6 @@ exports.index = function(req, res) {
   }
 };
 
-//
-//
 
 // GET /quizes/:id
 exports.show = function(req, res) {
@@ -98,6 +87,9 @@ exports.new = function(req, res) {
 // POST /quizes/create
 exports.create = function(req, res) {
   req.body.quiz.UserId = req.session.user.id;
+  if(req.files.image){
+    req.body.quiz.image = req.files.image.name;
+  }
   var quiz = models.Quiz.build( req.body.quiz );
 
   quiz
@@ -108,7 +100,7 @@ exports.create = function(req, res) {
         res.render('quizes/new', {quiz: quiz, errors: err.errors});
       } else {
         quiz // save: guarda en DB campos pregunta y respuesta de quiz
-        .save({fields: ["pregunta", "respuesta", "UserId"]})
+        .save({fields: ["pregunta", "respuesta", "UserId", "image"]})
         .then( function(){ res.redirect('/quizes')}) 
       }      // res.redirect: Redirección HTTP a lista de preguntas
     }
@@ -123,6 +115,9 @@ exports.edit = function(req, res) {
 
 // PUT /quizes/:id
 exports.update = function(req, res) {
+  if(req.files.image){
+    req.quiz.image = req.files.image.name;
+  }
   req.quiz.pregunta  = req.body.quiz.pregunta;
   req.quiz.respuesta = req.body.quiz.respuesta;
 
@@ -134,7 +129,7 @@ exports.update = function(req, res) {
         res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
       } else {
         req.quiz     // save: guarda campos pregunta y respuesta en DB
-        .save( {fields: ["pregunta", "respuesta"]})
+        .save( {fields: ["pregunta", "respuesta", "image"]})
         .then( function(){ res.redirect('/quizes');});
       }     // Redirección HTTP a lista de preguntas (URL relativo)
     }
